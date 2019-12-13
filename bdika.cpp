e7872429a029ac166c4d5dd43f6ca06145329859
ronen <ronenpresser@gmail.com>
2019-12-13 11:11:06 +0200

    added lexer.h

---
+++ bdika.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ronen on 13/12/2019.
+//
+
+#include "bdika.h"
---
+++ bdika.h
@@ -0,0 +1,14 @@
+//
+// Created by ronen on 13/12/2019.
+//
+
+#ifndef BIUADVANCEDPROG1_BDIKA_H
+#define BIUADVANCEDPROG1_BDIKA_H
+
+
+class bdika {
+
+};
+
+
+#endif //BIUADVANCEDPROG1_BDIKA_H

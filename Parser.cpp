d200086bd5c59255145c81df9eba7c8131a7ef8e
amittwito <amit6991@gmail.com>
2020-01-06 21:30:23 +0200

    comments, fixed last things.

--- Parser.cpp
+++ Parser.cpp
@@ -23,21 +23,13 @@ void executeCommandFunc(Command *c, vector<string> tokensVec, int currIndex, Par
 void Parser::parse(vector<string> tokensVec) {
   unsigned int index = 0;
   string token;
+
   //find the openDataServer command.
   auto it = find(tokensVec.begin(), tokensVec.end(), "openDataServer");
   if (it == tokensVec.end()) {
     cerr << "No openDataServer found" << endl;
     exit(EXIT_FAILURE);
   }
-  //find the connectControlClient command.
-  auto it2 = find(tokensVec.begin(), tokensVec.end(), "connectControlClient");
-  if (it2 == tokensVec.end()) {
-    cerr << "No connectControlClient found" << endl;
-    exit(EXIT_FAILURE);
-  }
-
-
-  //open the server with a thread
   //Get the index of the token.
   index = std::distance(tokensVec.begin(), it);
   //open the thread
@@ -46,9 +38,13 @@ void Parser::parse(vector<string> tokensVec) {
   tokensVec.erase(it);
   tokensVec.erase(it);
 
-
-
   //connect as a client with a thread
+  //find the connectControlClient command.
+  auto it2 = find(tokensVec.begin(), tokensVec.end(), "connectControlClient");
+  if (it2 == tokensVec.end()) {
+    cerr << "No connectControlClient found" << endl;
+    exit(EXIT_FAILURE);
+  }
   //Get the index of the token.
   index = std::distance(tokensVec.begin(), it2);
   //Open the thread
